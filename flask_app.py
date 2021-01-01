fe47f93d345dbf30ffb2e3a159482036d7d14479
jong2ucla <46505281+jong2ucla@users.noreply.github.com>
2021-01-01 14:43:44 -0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, request
 
-from processing import AATranslate, AltCodon #these are the two functions I defined that will input a protein coding DNA sequence and output (1) amino acids or (2) a synonymous DNA sequence.  
+from processing import AATranslate, AltCodon
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,35 +13,44 @@ app.config["DEBUG"] = True
 def mainpage():
     if request.method == "POST":
         InputDNA = None
-        InputDNA = request.form['UserInputDNA'] #"UserInputDNA' is what I called what the user will enter into the form.
-        if len(InputDNA)%3 == 0: #This checks to make sure the user's input DNA was a multiple of three. If so, it will perform three functions: (1) translate the user's sequence, (2) generate the synonymous DNA sequence, (2.5) translate the synonymous DNA sequence, and (3) check that the translation of the input user sequence and output synonymous sequence are the same.
-            TranslatedProtein = AATranslate(InputDNA) 
+        InputDNA = request.form['UserInputDNA']
+        if len(InputDNA)%3 == 0:
+            InputDNA = InputDNA.upper()
+            TranslatedProtein = AATranslate(InputDNA)
             RNAiResSeq = AltCodon(InputDNA)
             TransRNAiResSeq = AATranslate(RNAiResSeq)
+            ProtLength = len(TranslatedProtein)
+            InputDNALength = len(InputDNA)
         if TransRNAiResSeq == TranslatedProtein:
             return '''
                 <html>
                     <body>
-                        <p>Your input DNA is: {result0}</p>
-                        <p>The amino acid translation of your input DNA is: {result1}</p>
-                        <p>Your synonymous, RNAi-resistant DNA is: {result2}</p>
+                        <p>Your input DNA is:</p>
+                        <p>{result0}</p>
+                        <p>This input DNA sequence is <b>{result4}</b> base pairs. </p>
+                        <p>The amino acid translation of your input DNA is:</p>
+                        <p>{result1}</p>
+                        <p>This translation corresponds to <b>{result3}</b> residues. </p>
+                        <p>Your synonymous, RNAi-resistant DNA is:</p>
+                        <p>{result2}</p>
                         <p>This tool already checked to make sure the amino acid translation of the output DNA matched the translation of your input DNA.</p>
                         <p><a href="/">Click here to run another sequence </a>
                     </body>
                 </html>
-            '''.format(result0=InputDNA, result1=TranslatedProtein, result2=RNAiResSeq) #Here is where you tell the HTML what each thing in brackets should be
+            '''.format(result0=InputDNA, result1=TranslatedProtein, result2=RNAiResSeq, result3=ProtLength, result4=InputDNALength)
     return '''
         <html>
             <body>
-                <p><h1>Silent Mutation Generator</h1></p>
+                <p><h1>Synonymous Mutation Generator</h1></p>
                 <p><h2>About</h2></p>
                 <p><h3>This tool takes a protein coding DNA sequence and generates silent mutations (human codon optimized) to output a synonymous DNA sequence.</h3></p>
+                <p>This tool uses the standard codon table and does not consider the mitochondrial codon system.</p>
                 <p>If the input sequence is an RNAi target, this tool can be used to generate an RNAi-resistant sequence that preserves protein coding sequence.</p>
                 <p><h2>Usage</h2></p>
                 <p><h3>Enter your input sequence below.</h3></p>
                 <p>Your input sequence should be in all upper case, codon reading frame 1, and a multiple of 3.</p>
                 <p>Your input sequence should not have line breaks/spaces and should not have characters other than ACTG. It should also not contain a stop codon. </p>
-                <p><h3>This form does not (yet) check for invalid inputs, so double-check to make sure everything is formatted correctly.</h3></p>
+                <p><b>This form does not (yet) check for invalid inputs, so double-check to make sure everything is formatted correctly.</b></p>
                 <p>If there is a formatting error, the web page will crash instead of displaying an error message. Just reload and try again :) </p>
                 <form method="post" action=".">
                     <p><input name="UserInputDNA" /></p>
